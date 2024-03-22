a53b6b0afba8c5682019f4d972d643cdd303284c
AnnaD <Prominchyk@ukr.net>
2024-03-22 10:11:43 +0200

    db connect

--- db.php
+++ db.php
@@ -12,17 +12,5 @@ mysqli_query($link, "SET NAMES 'utf-8'");
 
 
 
-/*define('MODE', 'prod');
-
-$host = 'fdb1032.awardspace.net';
-$user = '4454954_forumdb';
-$pass = 'bmhV;Hbr3.LTH9t0';
-$name = '4454954_forumdb';
-
-$link = mysqli_connect($host, $user, $pass, $name);
-//mysqli_query($link, "SET NAMES 'utf-8'");*/
-
-
-
 
 ?>
